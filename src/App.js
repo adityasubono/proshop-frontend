e8d26f97dc48177ec5e90e26bead72166d3d3e58
Aditya Subono <adityasubono@gmail.com>
2021-03-29 09:55:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useEffect} from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Navbar from "./component/layout/Navbar";
+import Landing from "./component/layout/Landing";
+import Register from "./component/auth/Register";
+import Login from "./component/auth/Login";
+import Alert from "./component/layout/Alert";
+import { loadUser } from "./actions/auth";
+import Dashboard from "./component/dashboard/Dashboard";
+import PrivateRoute from "./component/routing/PrivateRoute";
+
+
+//Redux
+import { Provider } from "react-redux";
+import store from './store';
+import setAuthToken from "./utils/setAuthToken";
+
+if(localStorage.token) {
+    setAuthToken(localStorage.token);
 }
+
+
+const App = () => {
+
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
+
+
+
+    return (
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar/>
+                <Route exact path='/' component={Landing}/>
+                <section className='container'>
+                    <Alert/>
+                    <Switch>
+                        <Route exact path={'/register'} component={Register}/>
+                        <Route exact path={'/login'} component={Login}/>
+                        <PrivateRoute exact path={'/dashboard'} component={Dashboard}/>
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
+)};
+
+
+
+
 
 export default App;
